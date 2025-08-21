ccbf577f1a375ca745b3cf509fefda2ef4187e1a
Gabriel Rossi <Biel.invic@gmail.com>
2025-08-21 15:43:04 -0300

    Create teste_calculadora.py

---
+++ intro_git/teste_calculadora.py
@@ -0,0 +1,9 @@
+import calculadora
+
+a = 2
+b = 3
+
+print("a + b = ",calculadora.somar(a,b))
+print("a - b = ",calculadora.subtrair(a,b))
+print("a * b", calculadora.multiplicar(a,b))
+print("a / b = ",calculadora.dividir(a,b))
